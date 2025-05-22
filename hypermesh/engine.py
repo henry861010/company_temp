3b57f1b6af27f3805f26927160f4b0ec1b03e201
henry861010 <henry0978592955@gmail.com>
2025-05-23 02:55:48 +0800

    25/5/23

--- hypermesh/engine.py
+++ hypermesh/engine.py
@@ -13,19 +13,20 @@ layer
     
 layer
     z: float
-    area:
+    area: [
         type: BOX / CIRCLE / ...
         dim: [node1_x, node1_y, node3_x, node3_y] / [center_x, center_y, diameter] / ...
         hole: [hole, hole, ...]
         material: string
         metal: string
         density: 0.0~100.0
+    ]
 hole:
     type: BOX / CIRCLE / ...
     dim: [node1_x, node1_y, node3_x, node3_y] / [center_x, center_y, diameter] / ...
 '''
 
-def engine_get_z(obj, base_z) {
+def engine_get_z(obj, base_z, parent_obj) {
     layer_info_list = []
     
     ### get the main-object
@@ -34,7 +35,7 @@ def engine_get_z(obj, base_z) {
         ### get the hole
         hole = []
         for sub_obj in obj["obj"]:
-            thk = {item["thk"] for item in sub_obj}
+            thk = sum({layer["thk"] for layer in sub_obj})
             begin_z = base_z + sub_obj["z"]
             end_z = base_z + sub_obj["z"] + thk
             if begin_z <= z and z <= end_z:
@@ -56,23 +57,43 @@ def engine_get_z(obj, base_z) {
             ]
         })
         z += layer["thk"]
+        
     ### append the end layer info
+    ### connected-hole
+    hole = []
+    for sub_obj in parent_obj["obj"]:
+        thk = sum({layer["thk"] for layer in sub_obj["layer"]})
+        begin_z = base_z + sub_obj["z"]
+        if begin_z == z:
+            hole.append({
+                "type": sub_obj["type"],
+                "dim": sub_obj["dim"]
+            })
+            # ??? should add the dim identify
+    ### default material
+    temp_z = 0
+    for layer in parent_obj["layer"]:
+        if temp_z <= z and z <= (temp_z + layer["thk"]):
+            default_material = layer["material"]
+            break
+        temp_z += layer["thk"]
+        
     layer_info_list.append({
         "z": z,
         "area": [
             "type": obj["type"],
             "dim": obj["dim"],
-            "material": "DEFAULT",
-            "metal": "",
+            "material": ,
+            "metal": default_material,
             "density": 0,
-            "hole": []
+            "hole": hole
         ]
     })
         
     ### get the sub-object z info
     for sub_obj in obj["obj"]:
-        layer_info_list = layer_info_list + engine_get_z(sub_obj, base_z)
-        
+        temp_layer_info_list + engine_get_z(sub_obj, base_z, obj)
+        layer_info_list = layer_info_list + temp_layer_info_list
     return layer_info_list
 }
 
