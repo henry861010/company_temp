069d406a19bbbda3fc76ff3070d62197c43d6c77
henry861010 <henry0978592955@gmail.com>
2026-01-12 23:52:31 +0800

    26/1/12

--- mesher/connect/connect.py
+++ mesher/connect/connect.py
@@ -12,9 +12,26 @@ def is_tri_in_polygon(path_obj, node1, node2, node3):
 def simplify_outline(outline):
     return outline
 
+def cal_angle(p1, p2, p3):
+    # Create vectors relative to the middle node (p2)
+    u = np.array(p1) - np.array(p2)
+    v = np.array(p3) - np.array(p2)
+    
+    # Calculate dot product and magnitudes
+    dot_product = np.dot(u, v)
+    norm_u = np.linalg.norm(u)
+    norm_v = np.linalg.norm(v)
+    
+    # Calculate the cosine of the angle
+    # We clip the value to [-1, 1] to prevent tiny float errors from crashing acos
+    cosine_angle = np.clip(dot_product / (norm_u * norm_v), -1.0, 1.0)
+    
+    angle_rad = np.arccos(cosine_angle)
+    return np.degrees(angle_rad)
+
 class ConnectNode:
-    def __init__(self):
-        self.coord = []
+    def __init__(self, coord):
+        self.coord = np.array(coord, dtype=np.float32)
         self.neighbors = []
     
     def add_neighbor(self, neighbor:'ConnectNode'):
@@ -30,9 +47,12 @@ class ConnectNode:
             neighbor.neighbors.remove(self)
             
     def get_neighbor(self, ref_node:'ConnectNode'):
-        return self.neighbors[0]
-
-def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
+        if ref_node != self.neighbors[0]
+            return self.neighbors[0], cal_angle(self.neighbors[0].coord, self.coord, ref_node.coord)
+        else:
+            return self.neighbors[1], cal_angle(self.neighbors[1].coord, self.coord, ref_node.coord)
+    
+def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, outline, path_obj):
     def add(node2, node1, neighbor):
         '''
                 node2
@@ -64,14 +84,14 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
     
     ### check if neighbor1 is valid
     isNeighbor1 = True
-    if np.any(is_in_triangle(node1.coord, node2.coord, neighbor1.coord, simple_outline+[neighbor2]))
+    if np.any(is_in_triangle(node1.coord, node2.coord, neighbor1.coord, outline+[neighbor2]))
         isNeighbor1 = False
     elif not is_tri_in_polygon(path_obj, node1.coord, node2.coord, neighbor1.coord):
         isNeighbor1 = False
         
     ### check if neighbor2 is valid
     isNeighbor2 = True
-    if np.any(is_in_triangle(node1.coord, node2.coord, neighbor2.coord, simple_outline+[neighbor1]))
+    if np.any(is_in_triangle(node1.coord, node2.coord, neighbor2.coord, outline+[neighbor1]))
         isNeighbor2 = False
     elif not is_tri_in_polygon(path_obj, node1.coord, node2.coord, neighbor2.coord):
         isNeighbor2 = False
@@ -83,7 +103,7 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
             element_index = add(node2, node1, neighbor1)
 
             ### go next
-            if mesh_connect(mesh2d_obj, neighbor1, node2, simple_outline, path_obj)
+            if mesh_connect(mesh2d_obj, neighbor1, node2, outline, path_obj)
                 return True
 
             ### recorver
@@ -93,7 +113,7 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
             element_index = add(node1, node2, neighbor2)
             
             ### go next
-            if mesh_connect(mesh2d_obj, node1, neighbor2, simple_outline, path_obj)
+            if mesh_connect(mesh2d_obj, node1, neighbor2, outline, path_obj)
                 return True
 
             ### recorver
@@ -103,7 +123,7 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
         element_index = add(node2, node1, neighbor1)
         
         ### go next
-        if mesh_connect(mesh2d_obj, neighbor1, node2, simple_outline, path_obj)
+        if mesh_connect(mesh2d_obj, neighbor1, node2, outline, path_obj)
             return True
 
         ### recorver
@@ -113,7 +133,7 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
         element_index = add(node1, node2, neighbor2)
         
         ### go next
-        if mesh_connect(mesh2d_obj, node1, neighbor2, simple_outline, path_obj)
+        if mesh_connect(mesh2d_obj, node1, neighbor2, outline, path_obj)
             return True
 
         ### recorver
@@ -121,5 +141,33 @@ def mesh_connect(mesh2d_obj:'Mesh2D', node1, node2, simple_outline, path_obj):
         
     return False
         
+def connect(mesh2d_obj:'Mesh2D', outline):
+    outline = np.array(outline)
+    simple_outline = simplify_outline(outline)
+    path_obj = mpath.Path(outline)
     
-                
+    ### build node tree
+    node_list = [ConnectNode(outline[0])]
+    for node in outline[1:]:
+        node_obj = ConnectNode(node)
+        node_obj.add_neighbor(node_list[-1])
+        node_list.append(node_obj)
+    node_list[-1].add_neighbor(node_list[0])
+        
+    ### find begin node1 & node2
+    node1 = None
+    node2 = None
+    for i in range(len(node_list)):
+        p0 = node_list[i-2].coord
+        p1 = node_list[i-1].coord
+        p2 = node_list[i].coord
+        p3 = node_list[(i+1)%len(node_list)].coord
+        area1 = np.abs((p1[0] - p0[0]) * (p2[1] - p0[1]) - (p1[1] - p0[1]) * (p2[0] - p0[0]))
+        area2 = np.abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0]))
+        if area1 < 0.01 and area2 < 0.01:
+            node1 = node_list[i-1]
+            node2 = node_list[i]
+            break
+    
+    ### mesh
+    mesh_connect(mesh2d_obj, node1, node2, simple_outline, path_obj)             
